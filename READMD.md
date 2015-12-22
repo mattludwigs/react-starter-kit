90ec2604c40787f72e6feffae01ba5d51a311853
Matthew Ludwigs <matthew.ludwigs@colonyamerican.com>
2015-12-22 15:56:51 -0700

    init commit

---
+++ READMD.md
@@ -0,0 +1,25 @@
+# React Started Kit v0.0.1
+
+Some code to get you going on React projects
+
+This starter kit includes
+
+1. Webpack
+2. Webpack loaders for SASS
+3. Babel 6 support (stage 0)
+4. React
+5. eslint
+
+
+## Requirements
+1. Node v4+
+
+## Get Started
+
+```
+$ git clone 
+$ cd  
+$ npm install
+$
+```
+
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  port: 3000,
+  entry: path.join(__dirname, '/src/js/app.jsx'),
+  output: {
+    path: path.join(__dirname, '/out'),
+    filename: 'app.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    alias: {
+      components: path.join(__dirname, 'src/js/components')
+    }
+  },
+  watch: true,
+  plugins: [new HtmlWebpackPlugin({ filename: 'index.html', template : './src/index.html', inject: 'head' })],
+  module: {
+    loaders: [
+      {
+        test : /\.jsx|js$/,
+        loader : 'babel',
+        query : {
+          cacheDirectory: true,
+          presets: ['es2015', 'stage-2']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      }
+    ]
+  }
+}
